c0c50bc06b74cad969da4d9ccfd80dee682cea2c
203545 <34967853+203545@users.noreply.github.com>
2018-01-24 20:44:02 -0500

    Update Main.java

--- Player/src/Main.java
+++ Player/src/Main.java
@@ -8,7 +8,10 @@
  * - Movement now works, I fixed it by moving the if statement and putting the editing currentFloor above for loop
  * - If you want the long explanaion what what I did and why it worked you can ask me after school
  * TODO:
- * - Just add text events
+ * - LOOK AT LEVELS FILE
+ * - Fix erros
+ * - Create that method
+ * - May use backup as comparison
  */ 
 
 import java.util.ArrayList;
@@ -129,6 +132,7 @@ class Dalek
          myDirection = 4;
     }
 
+    //TODO: ADD COLLISSION
     // move in the current pointed direction
     public void move()
     {
@@ -155,53 +159,35 @@ class Dalek
 class World
 {
    private String[][] currentFloor;
-   private int myKey;
-   private int enemyY;
-   private int enemyX;
    
    public World()
    {
       currentFloor = Levels.getFloor1();
-      myKey = 0;
-      enemyY = 0;
-      enemyX = 0;
    }
    
-   public void setKey(int key)
+   public void checkNearbyEnemies()
    {
-      myKey = key;
-   }
-   
-   public void setEnemy(int Y, int X)
-   {
-      enemyY = Y;
-      enemyX = X;
-   }
-   
-   public void findEnemies(String[][] currentFloor)
-   {
-      int i = 0; // column number
-      int k = 0; // row number
-      int j;
-      
-      for (j = 0; j < (currentFloor[0].length * currentFloor.length); j++)
-      {   
-         if (currentFloor[k][i].equals("p"))
+      for (int z = 0; z < Floor1Enemies.length; z++)
+      {
+         if(Floor1Enemies[z][0] == myY && Floor1Enemies[z][1] == myX)
          {
-            setEnemy(k, i);
-            break;
-         }
-         
-         i++;
-         if (i == currentFloor[k].length)
-         {
-            i = 0;
-            k++;
+            System.out.println(Floor1Event[z]);
+            //Floor1Enemies[z][0] = null;
+            //Floor1Enemies[z][1] = null;
+            //Floor1Event[z] = null;
          }
       }
-   } 
-      
-      
+   }
+   
+   /*public void setFloor1Enemies(int X)
+    {
+      myX = X;
+    }
+    
+    public void setFloor1Event(int Y)
+    {
+      myY = Y;
+    } */
    
    public void display(int myDirection, int myY, int myX, int ifMove, Dalek dalek)
    { 
@@ -235,15 +221,7 @@ class World
          previousPosY = 1;
       }
       
-      findEnemies(currentFloor); //Keeps the location of and enemy, updates each time player moves
-       
-      if (currentFloor[myY][myX] == currentFloor[enemyY][enemyX])
-      {
-         System.out.println("You come into contact with bystander Tom. Before he can run, you use your plunger and he's a goner.");
-         currentFloor[myY][myX] = dalekPosition;
-         currentFloor[myY + previousPosY][myX + previousPosX] = " ";
-      }
-      else if ((ifMove == 1) && ((currentFloor[myY][myX].equals("_")) || (currentFloor[myY][myX].equals("|"))))
+      if ((ifMove == 1) && ((currentFloor[myY][myX].equals("_")) || (currentFloor[myY][myX].equals("|"))))
       {
          System.out.println("Make sure to stay within the walls!");
          currentFloor[myY + previousPosY][myX + previousPosX] = dalekPosition;
@@ -252,36 +230,6 @@ class World
          dalek.setX(myX);
          dalek.setY(myY);
       }
-      else if ((ifMove == 1) && (currentFloor[myY][myX].equals("c"))) //If player finds companion, the program sends a message and terminates
-      {
-         System.out.println("Congratulations, you found the companion!\nYou have obtained the key!");
-         currentFloor[myY][myX] = dalekPosition;
-         currentFloor[myY + previousPosY][myX + previousPosX] = " ";
-         previousPosY = 0;
-         previousPosX = 0;
-         setKey(1);
-      }
-      else if ((ifMove == 1) && (currentFloor[myY][myX].equals("s")))
-      {
-         currentFloor[myY][myX] = dalekPosition;
-         if (myKey == 1)
-         {
-            System.out.println("You have won!");
-            currentFloor[myY + previousPosY][myX + previousPosX] = " ";
-            previousPosY = 0;
-            previousPosX = 0;
-            System.exit(0); //Terminates program; temporary if we add in other levels
-         }
-        else
-        {
-         System.out.println("Woah there! You don't have the key yet!\nYou have to exterminate the companion still partner");
-         currentFloor[myY][myX] = "s";
-         myX = myX + previousPosX;
-         myY = myY + previousPosY;
-         dalek.setX(myX);
-         dalek.setY(myY);
-        }
-     }
       else if (ifMove == 1)
       {
          currentFloor[myY][myX] = dalekPosition;
@@ -295,6 +243,8 @@ class World
          previousPosY = 0;
          previousPosX = 0;
       } 
+      
+      
       
       
       //DO NOT EDIT ANYTHING UNDER HERE, DO NOT ADD ANYTHING, ANYTHING LIKE COLISSION SHOULD BE ADDED ABOVE
